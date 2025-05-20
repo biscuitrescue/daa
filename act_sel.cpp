9eecd6a7d1b3d63d43531b60101bdf4be8eba5b6
Karttikeya Sinha <biscuitrescue>
2025-05-20 10:20:21 +0530

    ass1

--- act_sel.cpp
+++ act_sel.cpp
@@ -4,43 +4,43 @@
 using namespace std;
 
 struct Activity {
-    int start, finish, index;
+  int start, finish, index;
 };
 
 bool activityCompare(const Activity &a1, const Activity &a2) {
-    return a1.finish < a2.finish;
+  return a1.finish < a2.finish;
 }
 
 void activitySelection(const vector<int> &start, const vector<int> &finish) {
-    int n = start.size();
-    vector<Activity> activities(n);
-    for (int i = 0; i < n; ++i) {
-        activities[i] = {start[i], finish[i], i+1}; // index+1 for 1-based indexing
+  int n = start.size();
+  vector<Activity> activities(n);
+  for (int i = 0; i < n; ++i) {
+    activities[i] = {start[i], finish[i], i+1}; // index+1 for 1-based indexing
+  }
+
+  sort(activities.begin(), activities.end(), activityCompare);
+
+  vector<int> selected;
+  int lastFinish = 0;
+
+  for (int i = 0; i < n; ++i) {
+    if (activities[i].start >= lastFinish) {
+      selected.push_back(activities[i].index);
+      lastFinish = activities[i].finish;
     }
+  }
 
-    sort(activities.begin(), activities.end(), activityCompare);
-
-    vector<int> selected;
-    int lastFinish = 0;
-
-    for (int i = 0; i < n; ++i) {
-        if (activities[i].start >= lastFinish) {
-            selected.push_back(activities[i].index);
-            lastFinish = activities[i].finish;
-        }
-    }
-
-    cout << "Maximum number of activities: " << selected.size() << endl;
-    cout << "Selected activities (1-based index): ";
-    for (int idx : selected) cout << idx << " ";
-    cout << endl;
+  cout << "Maximum number of activities: " << selected.size() << endl;
+  cout << "Selected activities (1-based index): ";
+  for (int idx : selected) cout << idx << " ";
+  cout << endl;
 }
 
 int main() {
-    vector<int> start = {1, 3, 0, 5, 8, 5};
-    vector<int> finish = {2, 4, 6, 7, 9, 9};
+  vector<int> start = {1, 3, 0, 5, 8, 5};
+  vector<int> finish = {2, 4, 6, 7, 9, 9};
 
-    activitySelection(start, finish);
+  activitySelection(start, finish);
 
-    return 0;
+  return 0;
 }
--- square.cpp
+++ square.cpp
@@ -4,43 +4,42 @@
 using namespace std;
 
 int maxSquareSubmatrix(const vector<vector<int>>& mat) {
-    int n = mat.size();
-    if (n == 0) return 0;
-    int m = mat[0].size();
+  int n = mat.size();
+  if (n == 0) return 0;
+  int m = mat[0].size();
 
+  vector<vector<int>> dp(n, vector<int>(m, 0));
 
-    vector<vector<int>> dp(n, vector<int>(m, 0));
+  int maxSide = 0;
 
-    int maxSide = 0;
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            if (mat[i][j] == 1) {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = 1;
-                } else {
-                    dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
-                }
-                maxSide = max(maxSide, dp[i][j]);
-            }
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < m; ++j) {
+      if (mat[i][j] == 1) {
+        if (i == 0 || j == 0) {
+          dp[i][j] = 1;
+        } else {
+          dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
         }
+        maxSide = max(maxSide, dp[i][j]);
+      }
     }
+  }
 
-    return maxSide;
+  return maxSide;
 }
 
 int main() {
-    vector<vector<int>> mat = {
-        {0, 1, 1, 0, 1},
-        {1, 1, 0, 1, 0},
-        {0, 1, 1, 1, 0},
-        {1, 1, 1, 1, 0},
-        {1, 1, 1, 1, 1},
-        {0, 0, 0, 0, 0}
-    };
+  vector<vector<int>> mat = {
+    {0, 1, 1, 0, 1},
+    {1, 1, 0, 1, 0},
+    {0, 1, 1, 1, 0},
+    {1, 1, 1, 1, 0},
+    {1, 1, 1, 1, 1},
+    {0, 0, 0, 0, 0}
+  };
 
-    int maxSide = maxSquareSubmatrix(mat);
-    cout << "Maximum length of square sub-matrix with all 1s: " << maxSide << endl;
+  int maxSide = maxSquareSubmatrix(mat);
+  cout << "Maximum length of square sub-matrix with all 1s: " << maxSide << endl;
 
-    return 0;
+  return 0;
 }
